fb70c1799a64ba86b3ac7417b6c03efb701473ac
Reza Mirjahanian <rmirjahanian@gmail.com>
2021-09-29 13:27:01 +0330

    Export Modules

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -3,18 +3,8 @@ import InfixToPrefix from "./InfixToPrefix";
 import PreFixCalc from "./PreFixCalc";
 
 
-const parser = new Parser()
-const toPrefix = new InfixToPrefix(parser);
-const calc = new PreFixCalc();
-
-//
-const prefix1 = toPrefix.convert('2+3+3  ');
-console.log(calc.run(prefix1)); // 8
-
-//
-const prefix2 = toPrefix.convert('2+3-3 ');
-console.log(calc.run(prefix2)); // 2
-
-//
-const prefix3 = toPrefix.convert('5 / 2 - 3');
-console.log(calc.run(prefix3)); //  -0.5
+export {
+  Parser,
+  InfixToPrefix,
+  PreFixCalc
+}
